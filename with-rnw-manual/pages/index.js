859c8bc51095f4013f61ce2d4fda8518d2302f79
Hemal Patil <hempat02@gmail.com>
2023-07-04 13:22:32 +0530

    Make with-rnw-manual play nice with temporary JS file

---
+++ with-rnw-manual/pages/index.js
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  link: {
+    color: 'blue',
+  },
+  textContainer: {
+    alignItems: 'center',
+    marginTop: 16,
+  },
+  text: {
+    alignItems: 'center',
+    fontSize: 24,
+    marginBottom: 24,
+  },
+})
+
+export default function App(props) {
+  return (
+    <View style={styles.container}>
+      <Text accessibilityRole="header" style={styles.text}>
+        Hello world rnw-manual
+      </Text>
+    </View>
+  )
+}
--- with-rnw-manual/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <main className="border border-red-600 p-4 rounded-lg">Hello world</main>
-  )
-}
